8e30ca95104afdbcb0e5ddaaeab150d99995b5b9
Leela-ui <leela912006@gmail.com>
2025-04-05 16:13:16 +0530

    ofijbgoij

---
+++ src/Components/ClassCom/index.js
@@ -0,0 +1,11 @@
+import React, {Component } from "react";
+
+export default class ClassCom extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            count: 0
+        }
+    }
+}
